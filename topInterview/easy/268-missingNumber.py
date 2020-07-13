5fe07ef922cef2d75790735a2a181edd18cc1821
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-07-13 12:25:11 +0600

    268 missing number

---
+++ topInterview/easy/268-missingNumber.py
@@ -0,0 +1,9 @@
+def missingNumber(nums):
+        n = len(nums)
+        sumNums = 0
+        sumSeries = n*(n+1)/2
+        
+        for num in nums:
+            sumNums += num
+        
+        return int(sumSeries-sumNums)
