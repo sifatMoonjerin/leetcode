96b5b96e8f214e83ddb9ba85fdfa506e9cccf0a0
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-07-14 07:24:49 +0600

    350 intersection

---
+++ topInterview/easy/350-intersectionArray.py
@@ -0,0 +1,13 @@
+def intersect(nums1, nums2):
+        count = {}
+        result = []
+        
+        for num in nums1:
+            count[num] = count.get(num, 0) + 1
+            
+        for num in nums2:
+            if num in count and count[num] > 0:
+                result.append(num)
+                count[num] -= 1
+                
+        return result
