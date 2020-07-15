b517b09f0d1dc0554ca2afe752230116130089f8
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-07-15 13:04:35 +0600

    1 two sum

---
+++ topInterview/easy/1-twoSum.py
@@ -0,0 +1,6 @@
+def twoSum(nums, target):
+        find = {}
+        for i in range(len(nums)):
+            if target-nums[i] in find:
+                return [find[target-nums[i]],i]
+            find[nums[i]] = i
