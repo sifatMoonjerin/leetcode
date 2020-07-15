59ffa1a3b7e2e6c0d8a121d7c65f77c2e8552f04
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-07-15 22:16:46 +0600

    169 updated

--- topInterview/easy/169-majorityElement.py
+++ topInterview/easy/169-majorityElement.py
@@ -1,15 +1,30 @@
 def majorityElement(nums):
-        count = {}
-        test = len(nums)/2
-        
-        for num in nums:
-            if num in count:
-                count[num] += 1
-            else:
-                count[num] = 1
-                
-            if count[num] > test:
-                return num
+    count = 1
+    majEl = nums[0]
+    
+    for num in nums[1:]:
+        if count == 0:
+            majEl = num
+            count += 1
+        elif majEl == num:
+            count += 1
+        else:
+            count -= 1
+    
+    return majEl
+
+    # count = {}
+    # test = len(nums)/2
+    
+    # for num in nums:
+    #     if num in count:
+    #         count[num] += 1
+    #     else:
+    #         count[num] = 1
+            
+    #     if count[num] > test:
+    #         return num
 
 
 print(majorityElement([1,2,3,2,1,1,1,1]))
+
