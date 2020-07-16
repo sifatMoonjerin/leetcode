54e06a41e2a1a22fbc54b77ed23390cd61fcfe03
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-07-16 09:57:28 +0600

    26 remove duplicated

---
+++ topInterview/easy/26-removeDuplicates.py
@@ -0,0 +1,12 @@
+#stupid problem
+def removeDuplicates(nums):
+        if not nums: return 0
+        
+        tail = 0
+        
+        for num in nums[1:]:
+            if num != nums[tail]:
+                tail += 1
+                nums[tail] = num
+        
+        return tail+1
