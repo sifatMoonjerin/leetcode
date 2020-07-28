82f25d3f23e53ff21a7972f7637c2e2b56ac2ff7
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-07-28 23:38:27 +0600

    1470 shuffle array

---
+++ easy/1470-shuffleArray.py
@@ -0,0 +1,10 @@
+def shuffle(nums, n):
+    newL = []
+    x, y = 0, n
+    for _ in range(0,n):
+        newL.append(nums[x])
+        newL.append(nums[y])
+        x += 1
+        y += 1
+    
+    return newL
